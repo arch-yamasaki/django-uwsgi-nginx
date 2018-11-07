7c216f45f4a4bb8008d63f0376f75d4c3da2c6e9
山崎陽平 <yohei7328@gmail.com>
2018-11-07 16:57:34 +0900

    change Static IP

--- app/website/settings.py
+++ app/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!@pm-wk*ry4*!=9*0frqbypu7@0c4u^%#*dsx0bz$$#6dr)1v_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["35.221.104.247", "127.0.0.1"]
+ALLOWED_HOSTS = ["35.190.235.117", "127.0.0.1"]
 
 
 # Application definition
